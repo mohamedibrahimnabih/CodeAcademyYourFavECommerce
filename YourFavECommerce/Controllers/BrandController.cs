25d25f0f7efcbb432c753b703749fcc820340b25
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-11-17 09:06:30 +0400

    Add Category Index

---
+++ YourFavECommerce/Controllers/BrandController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YourFavECommerce.Data;
+
+namespace YourFavECommerce.Controllers
+{
+    public class BrandController : Controller
+    {
+        private ApplicationDbContext _dbContext = new();
+
+        public IActionResult Index()
+        {
+            var brands = _dbContext.Brands.AsNoTracking().AsQueryable();
+
+            return View(brands.ToList());
+        }
+    }
+}
---
+++ YourFavECommerce/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YourFavECommerce.Data;
+
+namespace YourFavECommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.AsNoTracking().AsQueryable();
+
+            return View(categories.ToList());
+        }
+    }
+}
